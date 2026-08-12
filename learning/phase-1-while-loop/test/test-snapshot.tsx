dd0bc2a4664f20ce2bf25b081195e66e7c7ffc93
寸雨 <minghuazzz@qq.com>
2026-08-12 22:55:28 +0800

    chore(repo): keep learning prototypes local

--- learning/phase-1-while-loop/test/test-snapshot.tsx
+++
@@ -1,8 +0,0 @@
-import { render } from 'ink-testing-library';
-import React from 'react';
-import { App } from '../caturn.tsx';
-
-const { lastFrame, unmount } = render(React.createElement(App));
-console.log('=== 初始界面 ===');
-console.log(lastFrame());
-unmount();
